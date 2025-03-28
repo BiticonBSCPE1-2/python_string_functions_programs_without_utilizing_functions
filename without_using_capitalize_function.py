cfdecd58edd5b04a871f07f4bdba2c465666cfa4
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-28 22:19:04 +0800

    Properly capitalizes the input on the first letter while the rest are lower case

--- without_using_capitalize_function.py
+++ without_using_capitalize_function.py
@@ -1,2 +1,13 @@
 # capitalizes the first letter and lowers the rest
 # results the string with the first letter capitalized and the rest in lowercase
+
+def manual_capitalize(string):
+    if not string:
+        return ''
+    first = chr(ord(string[0]) - 32) if 'a' <= string[0] <= 'z' else string[0]
+    rest = ''.join(
+        chr(ord(char) + 32) if 'A' <= char <= 'Z' else char for char in string[1:])
+    return first + rest
+
+string = input("Enter a string: ")
+print(manual_capitalize(string))

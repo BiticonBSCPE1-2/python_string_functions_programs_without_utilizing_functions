5a223f4fa15ddd7429c8765ff33d7848b05fa7f7
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-28 22:10:12 +0800

    Adds spaces on both sides of the string properly to properly align to the center based on the user's input

--- without_using_center_function.py
+++ without_using_center_function.py
@@ -1,2 +1,10 @@
 # centers the string by adding spaces on both sides
 # results the string at the center with added spaces at both sides
+
+def manual_center(string, width):
+    space = max(0, width - len(string))
+    return ' ' * (space // 2) + string + ' ' * (space - space // 2)
+
+string = input("Enter a string: ")
+width = int(input("Enter desired width: "))
+print(manual_center(string, width))

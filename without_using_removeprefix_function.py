42979b492fc2340532975860b6c8c4b168f127a3
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-28 21:43:28 +0800

    Removes prefix manually properly

--- without_using_removeprefix_function.py
+++ without_using_removeprefix_function.py
@@ -1,2 +1,11 @@
 # removes the matching prefix from the string
 # results without the matching prefix from the string
+
+def manual_removeprefix(string, prefix):
+    if string.startswith(prefix):
+        return string[len(prefix):]
+    return string
+
+string = input("Enter a string: ")
+prefix = input("Enter prefix to remove: ")
+print(manual_removeprefix(string, prefix))

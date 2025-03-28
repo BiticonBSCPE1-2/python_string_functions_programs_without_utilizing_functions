d12d5123b86200dc5e060072029591e9832e87fc
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-28 21:51:02 +0800

    results true if string is all in upper case and false if not

--- without_using_isupper_function.py
+++ without_using_isupper_function.py
@@ -1,2 +1,8 @@
 # checks if all characters are uppercase
 # results True if all are uppercase, else False
+
+def manual_isupper(string):
+    return all(not ('a' <= char <= 'z') for char in string)
+
+string = input("Enter a string: ")
+print(manual_isupper(string))

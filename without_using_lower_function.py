810f3ff5c637aa12f9d6e2f71f8a02ee438ba042
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-28 21:46:50 +0800

    Automatically converts characters to lower case

--- without_using_lower_function.py
+++ without_using_lower_function.py
@@ -1,2 +1,8 @@
 # converts all characters to lowercase
 # results with all characters in lowercase
+
+def manual_lower(string):
+    return ''.join(chr(ord(char) + 32) if 'A' <= char <= 'Z' else char for char in string)
+
+string = input("Enter a string: ")
+print(manual_lower(string))

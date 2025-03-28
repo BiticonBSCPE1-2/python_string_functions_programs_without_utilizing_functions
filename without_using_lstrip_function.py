b78f645b02fd699de432b392cf9ddf107bd5213f
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-28 21:40:35 +0800

    Atuomatically removes the spaces at the beginning of the string

--- without_using_lstrip_function.py
+++ without_using_lstrip_function.py
@@ -1,2 +1,11 @@
 # removes the space at the beginning of the string
 # results without the spaces at the beginning of the string
+
+def manual_lstrip(string):
+    for index in range(len(string)):
+        if string[index] != ' ':
+            return string[index:]
+    return ''
+
+string = input("Enter a string: ")
+print(manual_lstrip(string))

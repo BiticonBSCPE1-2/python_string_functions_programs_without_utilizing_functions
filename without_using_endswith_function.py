245e5567922bf13a1a84ef7c6f26f4acbb5a41e0
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-03-28 21:54:35 +0800

    Results true if it ends with the inputted suffix and false if not

--- without_using_endswith_function.py
+++ without_using_endswith_function.py
@@ -1,2 +1,11 @@
 # checks if the string ends with a given suffix
 # results True if the string ends with the given suffix, else False
+
+def manual_endswith(string, suffix):
+    if len(suffix) <= len(string):
+        return string[-len(suffix):] == suffix
+    return False
+
+string = input("Enter a string: ")
+suffix = input("Enter suffix to check: ")
+print(manual_endswith(string, suffix))

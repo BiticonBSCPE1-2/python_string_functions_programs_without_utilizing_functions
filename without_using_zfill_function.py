33c71a40ba76124ade907216ecead927da3c6285
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-04-01 23:32:23 +0800

    Adds zeroes from the left based on the inputted width

--- without_using_zfill_function.py
+++ without_using_zfill_function.py
@@ -1,2 +1,9 @@
 # adds zeros to the left to make the string reach the given width
 # results the string with added zeros at the left
+
+def manual_zfill(string, width):
+    return '0' * max(0, width - len(string)) + string
+
+string = input("Enter a string: ")
+width = int(input("Enter desired width: "))
+print(manual_zfill(string, width))

915c4502a732ed701562922ab5b14ebb6baf1cd1
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-04-01 23:07:14 +0800

    Removes the spaces at the right side of the string

--- without_using_rstrip_function.py
+++ without_using_rstrip_function.py
@@ -1,2 +1,11 @@
 # removes the space at the end of the string
 # results without the spaces at the end of the string
+
+def manual_rstrip(string):
+    for index in range(len(string) - 1, -1, -1):
+        if string[index] != ' ':
+            return string[:index + 1]
+    return ''
+
+string = input("Enter a string: ")
+print(manual_rstrip(string))

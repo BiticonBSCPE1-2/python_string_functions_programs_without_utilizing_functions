dde7777d8db1c1b3e904ccffb0aaa1d1d943ab88
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-04-01 23:27:53 +0800

    Results true if the given prefix is correct and false if not

--- without_using_startswith_function.py
+++ without_using_startswith_function.py
@@ -1,2 +1,11 @@
 # checks if the string starts with a particular prefix
 # results True if the string starts with the prefix, False if not
+
+def manual_startswith(string, prefix):
+    if len(prefix) <= len(string):
+        return string[:len(prefix)] == prefix
+    return False
+
+string = input("Enter a string: ")
+prefix = input("Enter prefix to check: ")
+print(manual_startswith(string, prefix))

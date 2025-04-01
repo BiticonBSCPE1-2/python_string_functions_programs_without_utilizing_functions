6c2f5484f27ef146fce87db23946a0968ad78d4d
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-04-01 23:39:29 +0800

    Results the first position of the occurence of the substring in the inputted string

--- without_using_index_function.py
+++ without_using_index_function.py
@@ -1,2 +1,12 @@
 # finds the first occurrence of a substring
 # results the position of the first occurrence
+
+def manual_index(string, substring):
+    for i in range(len(string) - len(substring) + 1):
+        if string[i:i + len(substring)] == substring:
+            return i
+    return -1
+
+string = input("Enter a string: ")
+substring = input("Enter substring to find: ")
+print(manual_index(string, substring))

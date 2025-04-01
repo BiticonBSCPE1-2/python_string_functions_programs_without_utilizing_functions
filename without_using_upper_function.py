fb2318c343860f256c6d943e56e571e48404af8f
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-04-01 23:20:36 +0800

    Converts all lower case to upper case

--- without_using_upper_function.py
+++ without_using_upper_function.py
@@ -1,2 +1,9 @@
 # converts all characters to uppercase
 # results with all characters in uppercase
+
+def manual_upper(string):
+    return ''.join(
+        chr(ord(char) - 32) if 'a' <= char <= 'z' else char for char in string)
+
+string = input("Enter a string: ")
+print(manual_upper(string))

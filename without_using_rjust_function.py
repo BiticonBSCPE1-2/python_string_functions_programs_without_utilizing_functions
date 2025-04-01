b0243c6a09d6aeb40e7bc0da41c9e2e8003b9836
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-04-01 23:30:35 +0800

    Adds spaces according to the inputted width

--- without_using_rjust_function.py
+++ without_using_rjust_function.py
@@ -1,2 +1,9 @@
 # adds spaces to the left to make the string reach the given width
 # results the string with added spaces at the left
+
+def manual_rjust(string, width):
+    return ' ' * max(0, width - len(string)) + string
+
+string = input("Enter a string: ")
+width = int(input("Enter desired width: "))
+print(manual_rjust(string, width))

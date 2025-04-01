b1e142cf0bfd0e6e5138847a645311e224adb015
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-04-01 23:24:20 +0800

    Outputs false if there is an upper case letter in the string

--- without_using_islower_function.py
+++ without_using_islower_function.py
@@ -1,2 +1,8 @@
 # checks if all characters are lowercase
 # results True if all are lowercase, else False
+
+def manual_islower(string):
+    return all(not ('A' <= char <= 'Z') for char in string)
+
+string = input("Enter a string: ")
+print(manual_islower(string))

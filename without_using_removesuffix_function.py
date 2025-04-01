18657a7d9b63d9d2b14635e24fd49bd678bd2fb1
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-04-01 23:11:19 +0800

    Removes the suffix properly manually

--- without_using_removesuffix_function.py
+++ without_using_removesuffix_function.py
@@ -1,2 +1,11 @@
 # removes the suffix from the string
 # results without the suffix from the string
+
+def manual_removesuffix(string, suffix):
+    if string.endswith(suffix):
+        return string[:-len(suffix)]
+    return string
+
+string = input("Enter a string: ")
+suffix = input("Enter suffix to remove: ")
+print(manual_removesuffix(string, suffix))

228bfd0d4f47b43316537fb5ec0e397b92c52743
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-04-01 23:36:42 +0800

    Counts the occurences and results the number of occurences of a substring in the inputted string

--- without_using_count_function.py
+++ without_using_count_function.py
@@ -1,2 +1,13 @@
 # counts occurrences of a substring in the string
 # results the number of times the substring appears
+
+def manual_count(string, substring):
+    count = 0
+    for i in range(len(string) - len(substring) + 1):
+        if string[i:i + len(substring)] == substring:
+            count += 1
+    return count
+
+string = input("Enter a string: ")
+substring = input("Enter substring to count: ")
+print(manual_count(string, substring))
